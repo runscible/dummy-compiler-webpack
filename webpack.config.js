b5ba4412e6d9b16b4086d1371dd3901d95a55d81
runscible <runscible@gmail.com>
2020-07-26 02:25:49 -0300

    refactor O UNA VERSION QUE LE PUEDO PRESENTAR A MI MAMA

--- webpack.config.js
+++ webpack.config.js
@@ -9,59 +9,114 @@ function populateOptions() {
     return result
 }
 
-module.exports = async function(mode, argv) {
-    console.log('el valor de populate options es ', populateOptions())    
-    return {
-                entry: {
-                    ...populateOptions()
-                },
-                output: {
-                    path: path.resolve(__dirname, './dist'),
-                    filename: "[name].js"
-                },
-            module: {
-                rules: [
-                    {
-                        test: /\.js$/,
-                        exclude: /(node_modules)/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: ['@babel/preset-env']
-                            }
+const configPHP = () => (
+    {
+        entry: {
+            ...populateOptions()
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: "[name].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
                         }
-                    },
-                    {
-                        test: /\.(sa|sc|c)ss$/,
-                        use: [
-                            "style-loader",
-                            "css-loader",
-                            "sass-loader"
-                        ]
-                    },
-                    {
-                        test: /\.html$/,
-                        use: [
-                            {
-                                loader: "html-loader"
-                            }
-                        ]
                     }
-                ]
-            },
-                plugins: [
-                    new HtmlWebPackPlugin({
-                        template: "./src/index.html",
-                        filename: "./index.html",
-                        inlineSource: '.(js|css)$'
-                    }),
-                    new MiniCSSExtractPlugin({
-                        filename: "./src/**/*.scss"
-                    })
-                ],
-                mode: 'development'
-            }
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html",
+                inlineSource: '.(js|css)$'
+            }),
+            new MiniCSSExtractPlugin({
+                filename: "./src/**/*.scss"
+            })
+        ],
+        mode: 'development'
+    }
+) 
 
-
+const defaultConfig = () => (
+    {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: "[name].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html",
+                inlineSource: '.(js|css)$'
+            }),
+            new MiniCSSExtractPlugin({
+                filename: "./src/**/*.scss"
+            })
+        ],
+        mode: 'development'
+    }
+)
+module.exports = function() {
+    if (process.env.TYPE && process.env.TYPE == 'php') {
+        return configPHP()
+    } else {
+        return defaultConfig()
+    }
 }
-
+ 
